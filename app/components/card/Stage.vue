e8ab18e77703c511e02aabb80a76f3c3547935b6
Adrian Halliday <adrian.halliday@gmail.com>
2025-09-21 12:34:22 +1000

    chore: Refactor Draaggable into Card/index.vue

--- app/components/card/Stage.vue
+++ app/components/card/Stage.vue
@@ -1,251 +1,51 @@
-<script setup>
-import { ref, onMounted, onBeforeUnmount, nextTick } from "vue";
-const { $gsap, $Draggable } = useNuxtApp()
+<template>
+  <div class="card--stage" ref="stage">
+    <div class="card--drop--top" ref="discard"></div>
+    <div class="card--drop--left" ref="dislike"></div>
+    <div class="card--drop--centre" ref="centreRef"></div>
+    <div class="card--drop--right" ref="rightRef"></div>
+    <div class="card--drop--bottom" ref="bottomRef"></div>
 
-let draggableInstance = null;
-let startX = 0, startY = 0;
-let inner;
-const cardRef = ref(null);
+    <Card
+      :style="{ '--container-height': containerHeight }"
+      :stage="stage"
+      :discard="discard"
+      :dislike="dislike"
+      :centreRef="centreRef"
+      :rightRef="rightRef"
+      :bottomRef="bottomRef"
+      :containerHeight="containerHeight"
+      :containerWidth="containerWidth"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
 const stage = ref(null);
-const topRef = ref(null);
-const leftRef = ref(null);
+const discard = ref(null);
+const dislike = ref(null);
 const centreRef = ref(null);
 const rightRef = ref(null);
 const bottomRef = ref(null);
 const containerWidth = ref(0);
 const containerHeight = ref(0);
 
-const getAreas = () => {
-  return {
-    top:{
-      name: "top",
-      element: topRef.value,
-      innerOpacity: 1,
-      endTransform: {
-        opacity: 0,
-        x: startX,
-        y: startY - containerHeight.value / 2,
-        scale: 0.3,
-        rotation: 0,
-        duration: 0.5,
-        ease: "power2.out",
-      },
-    },
-    left:{
-      name: "left",
-      element: leftRef.value,
-      innerOpacity: 1,
-      endTransform: {
-        opacity: 0,
-        x: startX - containerWidth.value,
-        y: startY,
-        scale: 0.5,
-        rotation: -36,
-        duration: 0.7,
-        ease: "power2.out",
-      },
-    },
-    right:{
-      name: "right",
-      element: rightRef.value,
-      innerOpacity: 1,
-      endTransform: {
-        opacity: 0,
-        x: startX + containerWidth.value,
-        y: startY,
-        scale: 0.5,
-        rotation: +36,
-        duration: 0.7,
-        ease: "power2.out",
-      },
-    },
-    bottom:{
-      name: "bottom",
-      element: bottomRef.value,
-      innerOpacity: 1,
-      endTransform: {
-        opacity: 1,
-        x: startX,
-        y: (containerHeight.value / 2),
-        scale: 1.28,
-        rotation: 0,
-        duration: 0.5,
-        ease: "power2.out",
-      },
-    },
-    centre:{
-      name: "centre",
-      element: centreRef.value,
-      innerOpacity: 0,
-      endTransform: {
-        opacity: 1,
-        x: startX,
-        y: startY,
-        scale: 1,
-        rotation: 0,
-        duration: 0.5,
-        ease: "power2.out",
-      },
-    },
-  }
-};
-
 const updateDimensions = () => {
-  if (stage.value) {
-    containerWidth.value = stage.value.offsetWidth;
-    containerHeight.value = stage.value.offsetHeight;
-  }
-};
-
-const getAxis = (currentX, currentY) => {
-  if (currentX !== startX) return "x";
-  if (currentY !== startY) return "y";
-  return null;
-};
-
-const getDirection = (axis, currentX, currentY) => {
-  if (axis === "x") {
-    return currentX < startX ? "left" : "right";
-  } else if (axis === "y") {
-    return currentY < startY ? "top" : "bottom";
-  }
-  return null;
-};
-
-const handleDrag = (currentX, currentY) => {
-  const axis = getAxis(currentX, currentY);
-  const direction = getDirection(axis,currentX,currentY);
-
-  if (axis === "x") {
-    $gsap.to(cardRef.value.root, { rotation: currentX / 6, duration: 0.3 });
-
-    const opacity = Math.abs(currentX - startX) / (containerWidth.value / 2);
-    const element = inner?.querySelector(`.${direction}`);
-    if (element) {
-      $gsap.to(element, { opacity: Math.min(1, opacity), duration: 0.3 });
-    }
-  } else if (axis === "y") {
-    $gsap.to(cardRef.value.root, { scale: 1 + (currentY - startY) / 1000, duration: 0.3 });
-
-    const opacity = Math.abs(currentY - startY) / (containerHeight.value / 2);
-    const element = inner?.querySelector(`.${direction}`);
-    if (element) {
-      $gsap.to(element, { opacity: Math.min(1, opacity), duration: 0.3 });
-    }
-  }
-};
-
-const doHitTest = function(el) {
-  const hitbox = cardRef.value.root?.querySelector(".hitbox");
-  // console.log({
-  //   el: el,
-  //   hitbox,
-  //   cardRef: cardRef.value.root,
-  // })
-  const axis = getAxis(el.x, el.y);
-  const direction = getDirection(axis,el.x,el.y);
-
-  if (!hitbox) {
-    console.error("Hitbox not found!");
-    return;
-  }
-
-  const areas = getAreas();
-  const targetArea = areas[direction];
-  
-  if(targetArea) {
-    const hit = draggableInstance.hitTest(targetArea.element,hitbox, "-20%");
-
-    if(hit) {
-      console.warn(`You just hit ${targetArea.name}`);
-      $gsap.killTweensOf([cardRef.value.root, inner]);
-      $gsap.to(cardRef.value.root, targetArea.endTransform);
-      $gsap.to(inner.querySelectorAll("div"), {
-        duration: 0.5,
-        opacity: 0,
-        ease: "power2.out",
-      });
-    } else {
-      $gsap.killTweensOf([cardRef.value.root, inner]);
-      $gsap.to(cardRef.value.root, areas['centre'].endTransform);
-      $gsap.to(inner.querySelectorAll("div"), {
-        duration: 0.5,
-        opacity: 0,
-        ease: "power2.out",
-      });
-    }
-  } 
+  containerWidth.value = stage.value.offsetWidth;
+  containerHeight.value = stage.value.offsetHeight;
 };
 
 onMounted(async () => {
-  if ($gsap && !process.server) {
-    await nextTick()
-
-    if (!cardRef.value) {
-      console.error("refs are not yet mounted!");
-      return;
-    }
-
-    if (cardRef.value.root) {
-      inner = cardRef.value.innerRef;
-    }
-
-    const container = document.querySelector('.card--stage');
-    if (container) {
-      containerWidth.value = container.offsetWidth;
-      containerHeight.value = container.offsetHeight;
-    }
-
-    updateDimensions();
-    window.addEventListener('resize', updateDimensions);
-
-    draggableInstance = $Draggable.create(cardRef.value.root, {
-      type: "x,y",
-      lockAxis: true,
-      bounds: "body",
-      inertia: true,
-
-      onPress: function() {
-        startX = this.x;
-        startY = this.y;
-      },
-
-      onClick: () => console.warn("Click!"),
-
-      onDrag: function() {
-        handleDrag(this.x, this.y);
-      },
-
-      onDragEnd: function() {
-        doHitTest(this);
-      }
-    })[0];
-  }
-
+  updateDimensions();
+  window.addEventListener("resize", updateDimensions);
 });
 
 onBeforeUnmount(() => {
-  if (draggableInstance) {
-    draggableInstance.kill();
-  }
-  window.removeEventListener('resize', updateDimensions);
-  if (cardRef.value.root) {
-    $gsap.killTweensOf([cardRef.value.root, inner]);
-  }
+  window.removeEventListener("resize", updateDimensions);
 });
 </script>
-
-<template>
-  <div class="card--stage" ref="stage">
-    <div class="card--drop--top" ref="topRef"></div>
-    <div class="card--drop--left" ref="leftRef"></div>
-    <div class="card--drop--centre" ref="centreRef"></div>
-    <div class="card--drop--right" ref="rightRef"></div>
-    <div class="card--drop--bottom" ref="bottomRef"></div>
-
-    <Card ref="cardRef" :style="{ '--container-height': containerHeight }" />
-  </div>
-</template>
 
 <style lang="scss" scoped>
 .card {
@@ -273,7 +73,6 @@ onBeforeUnmount(() => {
     &--left {
       grid-area: left;
       border-right: 1px solid red;
-      
     }
     &--centre {
       grid-area: centre;
--- app/components/card/index.vue
+++ app/components/card/index.vue
@@ -1,26 +1,230 @@
 <template>
-  <div class="card" ref="root">
+  <div class="card" ref="cardRef">
     <div class="inner" ref="innerRef">
-      <div class="top">Top</div>
-      <div class="left">Left</div>
-      <div class="right">Right</div>
-      <div class="bottom">Bottom</div>
+      <!-- @TODO: make action-specific overlays -->
+      <div class="action-overlay discard">Discard</div>
+      <div class="action-overlay dislike">Dislike</div>
+      <div class="action-overlay favourite">Favourite</div>
+      <div class="action-overlay playlist">Playlist</div>
     </div>
     <div class="card--image">
       <NuxtImg src="/nuxt-icon.png" />
-      <div class="hitbox"></div>
+      <div class="hitbox" ref="hitBox"></div>
     </div>
     <div class="card--details"></div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount, nextTick } from "vue";
+const { $gsap, $Draggable } = useNuxtApp()
+const props = defineProps([
+  'stage',
+  'discard',
+  'dislike',
+  'centreRef',
+  'rightRef',
+  'bottomRef',
+  'containerHeight',
+  'containerWidth',
+]);
 
-const root = ref(null);
+let draggableInstance = null;
+let startX = 0, startY = 0;
+const cardRef = ref(null);
 const innerRef = ref(null);
+const hitBox = ref(null);
 
-defineExpose({root,innerRef});
+const getAreas = () => {
+  return {
+    discard:{
+      name: "discard",
+      element: props.discard,
+      innerOpacity: 1,
+      endTransform: {
+        opacity: 0,
+        x: startX,
+        y: startY - props.containerHeight / 2,
+        scale: 0.3,
+        rotation: 0,
+        duration: 0.5,
+        ease: "power2.out",
+      },
+    },
+    dislike:{
+      name: "dislike",
+      element: props.dislike,
+      innerOpacity: 1,
+      endTransform: {
+        opacity: 0,
+        x: startX - props.containerWidth,
+        y: startY,
+        scale: 0.5,
+        rotation: -36,
+        duration: 0.7,
+        ease: "power2.out",
+      },
+    },
+    favourite:{
+      name: "favourite",
+      element: props.rightRef,
+      innerOpacity: 1,
+      endTransform: {
+        opacity: 0,
+        x: startX + props.containerWidth,
+        y: startY,
+        scale: 0.5,
+        rotation: +36,
+        duration: 0.7,
+        ease: "power2.out",
+      },
+    },
+    playlist:{
+      name: "playlist",
+      element: props.bottomRef,
+      innerOpacity: 1,
+      endTransform: {
+        opacity: 1,
+        x: startX,
+        y: (props.containerHeight / 2),
+        scale: 1.28,
+        rotation: 0,
+        duration: 0.5,
+        ease: "power2.out",
+      },
+    },
+    centre:{
+      name: "centre",
+      element: props.centreRef,
+      innerOpacity: 0,
+      endTransform: {
+        opacity: 1,
+        x: startX,
+        y: startY,
+        scale: 1,
+        rotation: 0,
+        duration: 0.5,
+        ease: "power2.out",
+      },
+    },
+  }
+};
+
+const getAxis = (currentX, currentY) => {
+  if (currentX !== startX) return "x";
+  if (currentY !== startY) return "y";
+  return null;
+};
+
+const getAction = (axis, currentX, currentY) => {
+  if (axis === "x") {
+    return currentX < startX ? "dislike" : "favourite";
+  } else if (axis === "y") {
+    return currentY < startY ? "discard" : "playlist";
+  }
+  return null;
+};
+
+const handleDrag = (currentX, currentY) => {
+  const axis = getAxis(currentX, currentY);
+  const action = getAction(axis,currentX,currentY);
+
+  if (axis === "x") {
+    $gsap.to(cardRef.value, { rotation: currentX / 6, duration: 0.3 });
+
+    const opacity = Math.abs(currentX - startX) / (props.containerWidth / 2);
+    const element = innerRef.value.querySelector(`.${action}`);
+    if (element) {
+      $gsap.to(element, { opacity: Math.min(1, opacity), duration: 0.3 });
+    }
+  } else if (axis === "y") {
+    $gsap.to(cardRef.value, { scale: 1 + (currentY - startY) / 1000, duration: 0.3 });
+
+    const opacity = Math.abs(currentY - startY) / (props.containerHeight / 2);
+    const element = innerRef.value.querySelector(`.${action}`);
+    if (element) {
+      $gsap.to(element, { opacity: Math.min(1, opacity), duration: 0.3 });
+    }
+  }
+};
+
+const doHitTest = function(el) {
+  const axis = getAxis(el.x, el.y);
+  const action = getAction(axis,el.x,el.y);
+
+  const areas = getAreas();
+  const targetArea = areas[action];
+  
+  if(targetArea) {
+    const hit = draggableInstance.hitTest(targetArea.element,hitBox.value, "20%");
+
+    if(hit) {
+      $gsap.killTweensOf([cardRef.value, innerRef]);
+      // Tween to final position based on selected direction
+      $gsap.to(cardRef.value, targetArea.endTransform);
+      $gsap.to(innerRef.value.querySelectorAll("div"), {
+        duration: 0.5,
+        opacity: 0,
+        ease: "power2.out",
+      });
+    } else {
+      $gsap.killTweensOf([cardRef.value, innerRef]);
+      // Reset to middle position if no hit
+      $gsap.to(cardRef.value, areas['centre'].endTransform);
+      $gsap.to(innerRef.value.querySelectorAll("div"), {
+        duration: 0.5,
+        opacity: 0,
+        ease: "power2.out",
+      });
+    }
+  } 
+};
+
+onMounted(async () => {
+  if ($gsap && !process.server) {
+    await nextTick()
+
+    if (!cardRef.value) {
+      console.error("refs are not yet mounted!");
+      return;
+    }
+
+    draggableInstance = $Draggable.create(cardRef.value, {
+      type: "x,y",
+      lockAxis: true,
+      trigger: cardRef.value,
+      // @TODO: Finalise bounds relative to mobile viewport
+      bounds: "body",
+      inertia: true,
+
+      onPress: function() {
+        startX = this.x;
+        startY = this.y;
+      },
+
+      onClick: () => console.warn("Click!"),
+
+      onDrag: function() {
+        handleDrag(this.x, this.y);
+      },
+
+      onDragEnd: function() {
+        doHitTest(this);
+      }
+    })[0];
+  }
+
+});
+
+onBeforeUnmount(() => {
+  if (draggableInstance) {
+    draggableInstance.kill();
+  }
+  if (cardRef.value) {
+    $gsap.killTweensOf([cardRef.value, innerRef]);
+  }
+});
+
 </script>
 
 <style lang="scss" scoped>
@@ -37,20 +241,23 @@ defineExpose({root,innerRef});
     aspect-ratio: 16/9;
     background-color: darkgreen;
     position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
 
     img {
-      max-width: 100%;
-      height: 100%;
+      position: absolute;
+      inset: 0;
       object-fit: cover;
       object-position: center;
     }
 
     .hitbox {
-      width: 30%;
-      aspect-ratio: 1;
+      width: 80px;
+      height: 80px;
       position: absolute;
       bottom: 0;
-      left: 35%;
       border-radius: 50%;
     }
   }
@@ -67,17 +274,27 @@ defineExpose({root,innerRef});
     inset: 0;
     z-index: 10;
     
-    > div {
+    .action-overlay {
       position: absolute;
       inset: 0;
-      width: 100%;
-      height: 100%;
-      background-color: black;
       opacity: 0;
       display: flex;
       align-items: center;
       justify-content: center;
-      color: white;
+      color: var(--c-white);
+
+      &.discard {
+        background-color: var(--c-action-discard);
+      }
+      &.dislike {
+        background-color: var(--c-action-dislike);
+      }
+      &.favourite {
+        background-color: var(--c-action-favourite);
+      }
+      &.playlist {
+        background-color: var(--c-action-playlist);
+      }
     }
   }
 }
